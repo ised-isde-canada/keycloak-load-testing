01b4044f36a05e2726caaa9585a72a537119c65a
Sagal Maxamud <smaxa028@uottawa.ca>
2020-06-18 15:00:37 -0400

    Removing open test report componenet

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,44 +12,44 @@ object Main extends App{
 
     runLoadTest()
 
-    // get current directory
-    val currentDirectory = new java.io.File("./target/gatling/").getCanonicalPath()
+    // // get current directory
+    // val currentDirectory = new java.io.File("./target/gatling/").getCanonicalPath()
 
-    // get folders for reports.. need to figure out how to get most recent one
-    val folders = getListOfFolders(currentDirectory)
+    // // get folders for reports.. need to figure out how to get most recent one
+    // val folders = getListOfFolders(currentDirectory)
     
 
-    //get most recent folder from list
-    val recent = folders.sorted.last.getCanonicalPath()
-    // System.out.println("Folders :" + folders.sorted.last)
+    // //get most recent folder from list
+    // val recent = folders.sorted.last.getCanonicalPath()
+    // // System.out.println("Folders :" + folders.sorted.last)
     
-    // get list of files from recent folder
-    val files = getListOfFiles(recent)
-    // System.out.println("Files : " + files)
+    // // get list of files from recent folder
+    // val files = getListOfFiles(recent)
+    // // System.out.println("Files : " + files)
 
-    //html file of most recent report
-    val html = files(0)
+    // //html file of most recent report
+    // val html = files(0)
 
-    // open report in computer's default browser
-    Desktop.getDesktop().browse(html.toURI())
+    // // open report in computer's default browser
+    // Desktop.getDesktop().browse(html.toURI())
 
-    def getListOfFiles(dir: String):List[File] = {
-        val d = new File(dir)
-        if (d.exists && d.isDirectory) {
-            d.listFiles.filter(_.isFile).toList
-        } else {
-            List[File]()
-        }
-    }
+    // def getListOfFiles(dir: String):List[File] = {
+    //     val d = new File(dir)
+    //     if (d.exists && d.isDirectory) {
+    //         d.listFiles.filter(_.isFile).toList
+    //     } else {
+    //         List[File]()
+    //     }
+    // }
 
-    def getListOfFolders(dir: String):List[File] = {
-        val d = new File(dir)
-        if (d.exists && d.isDirectory) {
-            d.listFiles.filter(_.isDirectory).toList
-        } else {
-            List[File]()
-        }
-    }
+    // def getListOfFolders(dir: String):List[File] = {
+    //     val d = new File(dir)
+    //     if (d.exists && d.isDirectory) {
+    //         d.listFiles.filter(_.isDirectory).toList
+    //     } else {
+    //         List[File]()
+    //     }
+    // }
     
     def runLoadTest() = {
         "sbt gatling:test".!
